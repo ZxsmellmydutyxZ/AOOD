338225526735a2c5f885d182a1efce2fde0f8790
ebarne0781 <ebarne0781@AHHDB11C6BFAAF.inst.hcpss.org>
2015-09-30 10:09:08 -0400

    ffffffff

---
+++ MemoryGame/src/GameProperties.java
@@ -0,0 +1,45 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import java.awt.CardLayout;
+
+
+public class GameProperties extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameProperties frame = new GameProperties();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GameProperties() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 486, 186);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
+	}
+
+}
