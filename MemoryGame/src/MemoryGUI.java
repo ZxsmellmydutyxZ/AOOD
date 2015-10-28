4ecab6063d167a934cb5f1dcb04c43678bc6999f
DuTy <DuTy@carl.mshome.net>
2015-10-27 22:35:33 -0400

    d

--- MemoryGame/src/MemoryGUI.java
+++ MemoryGame/src/MemoryGUI.java
@@ -18,23 +18,26 @@ public class MemoryGUI implements ActionListener{
 	private JTable table;
 	static Board board = new Board();
 	static int turn = 1;
-	
+	static int playerturn = turn-1;
 	static int cardsflipped = 0;
-	int matches = 0;
+	static String matches = "1";
 	static int playercounter = 0;
 	int matchlimit = 0;
 	static ArrayList<Card> pickedcards = new ArrayList<Card>();
 	
 	public static int pick = 0;
-	public ArrayList<Player> players = new ArrayList<Player>();
+	public static ArrayList<Player> players = new ArrayList<Player>();
 	JLabel lblTurn;
-	JLabel lblNewLabel;
-	JLabel lblPlayer;
-	JLabel lblStats;
-	
+	static JLabel lblNewLabel;
+	static JLabel lblStats;
+	static JLabel lblNewLabel_1;
 	//public static Card[][] cards;
 	public int r = 3;
 	public int c = 6;
+	private JLabel lblScore;
+	
+	
+	
 	
 
 	/**
@@ -44,6 +47,7 @@ public class MemoryGUI implements ActionListener{
 		initialize();
 		
 		
+		
 	}
 	
 
@@ -51,6 +55,10 @@ public class MemoryGUI implements ActionListener{
 	 * Initialize the contents of the frame.
 	 */
 	public void initialize() {
+		
+		
+		
+		
 		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 771, 420);
@@ -62,23 +70,26 @@ public class MemoryGUI implements ActionListener{
 		frame.getContentPane().add(panel);
 		createGrid(panel, 3, 6);
 		
-		
-		lblPlayer = new JLabel("Player");
-		lblPlayer.setBounds(20, 44, 46, 14);
-		frame.getContentPane().add(lblPlayer);
-		
-		lblNewLabel = new JLabel(String.valueOf(turn));
-		lblNewLabel.setBounds(60, 44, 46, 14);
+		lblNewLabel = new JLabel("Player 0's ");
+		lblNewLabel.setBounds(10, 44, 66, 14);
 		frame.getContentPane().add(lblNewLabel);
 		
 		
 		lblTurn = new JLabel("turn.");
-		lblTurn.setBounds(116, 44, 46, 14);
+		lblTurn.setBounds(86, 44, 46, 14);
 		frame.getContentPane().add(lblTurn);
 		
-		lblStats = new JLabel("New label");
-		lblStats.setBounds(644, 69, 46, 14);
+		lblStats = new JLabel(" ");
+		lblStats.setBounds(644, 88, 46, 14);
 		frame.getContentPane().add(lblStats);
+		
+		lblNewLabel_1 = new JLabel("Player 0's ");
+		lblNewLabel_1.setBounds(637, 44, 63, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		lblScore = new JLabel("score.");
+		lblScore.setBounds(699, 44, 46, 14);
+		frame.getContentPane().add(lblScore);
 		
 		
 		
@@ -114,7 +125,7 @@ public class MemoryGUI implements ActionListener{
 	        
 	        
 	        cardcard.getButton().addActionListener(new ActionListener(){
-	        
+
 	      
 	        
 	       public void actionPerformed(ActionEvent evt) {
@@ -159,19 +170,28 @@ public class MemoryGUI implements ActionListener{
 	        		System.out.println(pickedcards.size());
 	        		
 	        		if(pickedcards.size()>1){
+	        			
 	        			if(pickedcards.get(0).check(pickedcards.get(1))){
 	        				System.out.println("wow");
+
+		        				players.get(playerturn).addMatch();
+		        				matches = String.valueOf(players.get(turn-1).getMatches());
+		        				lblStats.setText(matches);
 	        				
 	        			}
+	        			if(++turn>playercounter+1){
+	        			turn = 1;
+	        			
+	        			}
+	        	        
+	        			
+	        			
+	        			lblNewLabel.setText(players.get(turn-1).getName() + "'s");
+	        			lblNewLabel_1.setText(players.get(turn-1).getName() + "'s");
 	        			
 	        		}
 	        }else{
-	        	for(;turn<playercounter;){
-	        	turn++;
-	        	}
-	        	if(turn == playercounter){
-	        	turn = 0;
-	        	}
+	        	
 	        	 
 	        	for (int c = 0; c < 3; c++)
 	    	    {
@@ -207,22 +227,23 @@ public class MemoryGUI implements ActionListener{
 	 * Launch the application.
 	 */
 	
-	public void Start(int playercount, int matchlimit){
+public void Start(int playercount, int matchlimit){
 		
 		playercounter = playercount;
 		
 		MemoryGUI window = new MemoryGUI();	
 		window.frame.setVisible(true);
 			
+		
 		for(int i = 0; i <= playercounter;i++){
-			 
-			String hh = Integer.toString(i);
+
+			String hh = "Player " + Integer.toString(i);
+			
 			Player e = new Player();
 			e.setName(hh);
 			players.add(e);
-			System.out.println(players.get(i).getName() );
+			
 		}
-		
 		
 		
 	}
